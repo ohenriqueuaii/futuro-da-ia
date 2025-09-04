0a4ccdb404624710457978e52cc2b78acc27faaf
Bigodinho <145065087+ohenriqueuaii@users.noreply.github.com>
2025-09-04 16:23:20 -0300

    ...

---
+++ script.js
@@ -0,0 +1,51 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+        "1. A inteligência artificial pode aprender a partir de dados sem intervenção humana?
+        alternativas[
+           ( ) Sim
+           ( ) Não
+        ]
+    }
+[
+    {
+         enunciado:
+        A IA é capaz de substituir completamente os seres humanos em todas as funções de trabalho?
+         alternativas[
+           ( ) Sim
+           ( ) Não
+    ]
+}
+[
+{ 
+    enunciado: 
+        Os algoritmos de IA podem tomar decisões autônomas?
+        alternativas[
+           ( ) Sim
+           ( ) Não
+]
+}
+[
+{
+    enuniado:
+        A IA pode entender emoções humanas com a mesma profundidade que um ser humano?
+        alternativas[
+           ( ) Sim
+           ( ) Não
+]
+}
+[
+{
+    enunciado:
+        A inteligência artificial é capaz de criar novos conhecimentos sem a ajuda de humanos?
+        alternativas[
+           ( ) Sim
+           ( ) Não
+]
+}
