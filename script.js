96ad13dda770d9741ae124325ad3060805087233
Bigodinho <145065087+ohenriqueuaii@users.noreply.github.com>
2025-10-23 16:25:40 -0300

    .

--- script.js
+++ script.js
@@ -49,3 +49,30 @@ const perguntas = [
            ( ) Não
 ]
 }
+
+function mostraPergunta() {
+caixaPerguntas.textContent perguntaAtual.enunciado;
+caixaAlternativas.textContent = "";
+}
+
+mostraAlternativas();
+function mostraAlternativas(){
+for(const alternativa of perguntaAtual.alternativas) {
+}
+}
+const botaoAlternativas document.createElement("button");
+botaoAlternativas.textContent alternativa.texto;
+botaoAlternativas.addEventListener("click", () -> respostaseleciona
+caixaAlternativas.appendChild(botaoAlternativas);
+function respostaselecionada (opcaoSelecionada) {
+const afirmacoes opcaoSelecionada.afirmacao;
+historiaFinal + afirmacoes + ";
+atual++;
+mostraPergunta();
+}
+function mostraResultado() {
+caixaPerguntas.textContent "Em 2049...";
+textoResultado.textContent historiaFinal;
+caixaAlternativas.textContent = "";
+}
+mostraPergunta();
